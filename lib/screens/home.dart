72e4f83618775afc5918686602f7b1a11add1d0a
pla <mewebsure@gmail.com>
2020-05-06 10:53:47 +0700

    Design UI home Success

---
+++ lib/screens/home.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:plajob/utility/my_style.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+// Field
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: RadialGradient(
+              colors: <Color>[Colors.white, MyStyle().primaryColor],
+              radius: 1.3,
+              center: Alignment(0, -0.5),
+            ),
+          ),
+          child: Center(
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  showLogo(),
+                  MyStyle().mySizeBox(),
+                  showAppName(),
+                  MyStyle().mySizeBox(),
+                  userForm(),
+                  MyStyle().mySizeBox(),
+                  passForm(),
+                  MyStyle().mySizeBox(),
+                  loginButton(),
+                  registerButton(),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  FlatButton registerButton() => FlatButton(
+        onPressed: () {},
+        child: Text(
+          'New Register',
+          style: TextStyle(color: Colors.blue),
+        ),
+      );
+
+  Widget loginButton() => Container(
+        width: 250.0,
+        child: RaisedButton(
+          color: MyStyle().darkColor,
+          onPressed: () {},
+          child: Text(
+            'Login',
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+      );
+
+  Container passForm() {
+    return Container(
+      width: 250.0,
+      child: TextField(
+        obscureText: true,
+        decoration: InputDecoration(
+          labelText: 'Password :',
+          hintText: 'Password',
+          border: OutlineInputBorder(),
+        ),
+      ),
+    );
+  }
+
+  Container userForm() {
+    return Container(
+      width: 250.0,
+      child: TextField(
+        decoration: InputDecoration(
+            labelStyle: TextStyle(color: MyStyle().darkColor),
+            labelText: 'User Name :',
+            enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: MyStyle().darkColor)),
+            focusedBorder: OutlineInputBorder()),
+      ),
+    );
+  }
+
+  Text showAppName() => Text(
+        'PLA FIND JOBS',
+        style: GoogleFonts.athiti(
+            textStyle: TextStyle(
+          fontSize: 25.0,
+          fontWeight: FontWeight.bold,
+          fontStyle: FontStyle.italic,
+          color: MyStyle().darkColor,
+        )),
+      );
+
+  Container showLogo() {
+    return Container(
+      width: 120.0,
+      child: Image.asset('images/logo.png'),
+    );
+  }
+}
---
+++ lib/utility/my_style.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MyStyle {
+  // Field
+Color primaryColor = Color.fromARGB(0xff, 0xff, 0x8f, 0x00);
+Color darkColor = Color.fromARGB(0xff, 0xff, 0xca, 0x28);
+  // Method
+
+  SizedBox mySizeBox() => SizedBox(
+        height: 16.0,
+      );
+
+  MyStyle();
+}
